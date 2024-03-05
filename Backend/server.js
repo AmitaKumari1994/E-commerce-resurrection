7a53346aa0e041e2f243fbc44d94cc9fc1415855
AmitaKumari1994 <80741439+AmitaKumari1994@users.noreply.github.com>
2024-03-05 16:51:13 +0530

    repository updated

--- Backend/server.js
+++ Backend/server.js
@@ -44,6 +44,21 @@ app.use('/api/upload',uploadRoutes)
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+//For production
+if(process.env.NODE_ENV==='production'){
+    //set static folder
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    //any route that is not api will be redirected to index.html
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    })
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is running.....');
+    })
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
