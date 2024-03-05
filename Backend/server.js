ac54517b755ee826c9e997d77c56e1e3313b6001
AmitaKumari1994 <80741439+AmitaKumari1994@users.noreply.github.com>
2024-03-05 20:51:05 +0530

    removed brackets

--- Backend/server.js
+++ Backend/server.js
@@ -50,9 +50,9 @@ if(process.env.NODE_ENV==='production'){
     app.use(express.static(path.join(__dirname,'/frontend/build')));
 
     //any route that is not api will be redirected to index.html
-    app.get('*',(req,res)=>{
+    app.get('*',(req,res)=>
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
+    )
 }else{
     app.get('/',(req,res)=>{
         res.send('API is running.....');
